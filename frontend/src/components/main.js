def2718390883110f60a2a6a719cef1df9c91d4f
Farid Gasimov <faridgasimov.93@gmail.com>
2024-09-27 18:21:37 +0400

    first version of the project

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -1,1 +1,27 @@
-console.log('hello');
+function dropDownMenu() {
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+
+    if (dropdownToggle) {
+        dropdownToggle.addEventListener('click', function (e) {
+            e.preventDefault();
+            this.classList.toggle('clicked');
+
+            const dropdownMenu = this.nextElementSibling;
+            if (dropdownMenu.style.display === 'block') {
+                dropdownMenu.style.display = 'none';
+            } else {
+                dropdownMenu.style.display = 'block';
+            }
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    dropDownMenu();
+});
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    const sidebar = document.querySelector('.col-2');
+    const menuButton = document.querySelector('.menu-toggle');
+    sidebar.classList.toggle('show-sidebar');
+    menuButton.classList.toggle('show-sidebar');
+});
